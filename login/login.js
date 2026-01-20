0a3951c6d509788184a58ac2d056164a1ec38346
MineHR Solutions <harshchavda980@gmail.com>
2026-01-20 17:58:09 +0530

    Add password toggle and login alert functions

---
+++ login/login.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+  const pass = document.getElementById("password");
+  pass.type = pass.type === "password" ? "text" : "password";
+}
+
+function login() {
+  alert("Login functionality will be connected to backend.");
+}
